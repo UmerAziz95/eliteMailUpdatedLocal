cfb7d616f2b2e97f9ad14b02a64b6b72ed73be6b
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2025-04-25 12:40:46 +0500

    updatingAt

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,7 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('
 Route::post('/change-password', [AuthController::class, 'changePassword'])->name('change.password')->middleware('auth');
 
 Route::middleware(['role:1,2,5'])->prefix('admin')->name('admin.')->group(function () {
+    //listing routes
        Route::middleware('view.only')->group(function () {
         Route::get('/', [AdminController::class, 'index'])->name('index');
         Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
