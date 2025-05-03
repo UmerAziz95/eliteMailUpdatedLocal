45b8ac2d5aa339315c533dbb8e9c0e360dc912b9
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-05-03 16:54:59 +0500

    2025-03-05

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CustomRoleSeeder::class,
             RolesTableSeeder::class,
             HostingPlatformSeeder::class,
             SendingPlatformSeeder::class,
