570c2e382fbee7678ab55833a71d0ba232062609
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-05-05 10:55:40 +0500

    Added Statuses Table and Seeder

--- database/seeders/StatusesTableSeeder.php
+++ database/seeders/StatusesTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\Status;
 
 class StatusesTableSeeder extends Seeder
 {
@@ -11,14 +11,19 @@ class StatusesTableSeeder extends Seeder
     {
         $statuses = [
             ['name' => 'pending', 'badge' => 'warning'],
-            ['name' => 'in-approval', 'badge' => 'warning'],
-            ['name' => 'approved', 'badge' => 'success'],
+            ['name' => 'in-approval', 'badge' => 'info'],
+            ['name' => 'approved', 'badge' => 'light'],
             ['name' => 'reject', 'badge' => 'secondary'],
             ['name' => 'in-progress', 'badge' => 'primary'],
             ['name' => 'cancelled', 'badge' => 'danger'],
             ['name' => 'completed', 'badge' => 'success'],
         ];
 
-        DB::table('statuses')->insert($statuses);
+        foreach ($statuses as $status) {
+            Status::updateOrCreate(
+                ['name' => $status['name']],   // Match by name
+                ['badge' => $status['badge']]  // Update badge if exists
+            );
+        }
     }
 }
