ebf328cccb4eb66206103e20058014ca462f2c1d
5D Developer <127947055+5ddeveloper247@users.noreply.github.com>
2025-06-19 11:57:26 +0500

    asd

--- resources/views/customer/public_outside/plans.blade.php
+++ resources/views/customer/public_outside/plans.blade.php
@@ -14,12 +14,31 @@
             line-height: 1.2;
         }
 
-        header {
+        section {
+            position: relative;
+            height: 100%;
+            width: 100%;
+        }
+
+        section::after {
+            content: '';
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            top: -56%;
+            left: 0%;
             background-image: url('https://cdn.prod.website-files.com/68271f86a7dc3b457904455f/682746cfd17013f9e1df8695_Qi3BAFZ1SJONCSiPk3u43ulmBhw.png');
             background-position: 50% 80%;
             background-size: 65%;
             background-repeat: no-repeat;
         }
+
+        /* header {
+            background-image: url('https://cdn.prod.website-files.com/68271f86a7dc3b457904455f/682746cfd17013f9e1df8695_Qi3BAFZ1SJONCSiPk3u43ulmBhw.png');
+            background-position: 50% 80%;
+            background-size: 65%;
+            background-repeat: no-repeat;
+        } */
 
         p {
             color: var(--light-color);
@@ -171,111 +190,113 @@
     </style>
 @endpush
 @section('content')
-    <div class="container py-3">
-        <div id="subscription-loader" class="d-none position-absolute top-50 start-50 translate-middle text-center"
-            style="z-index: 9999; min-width: 200px;">
-            <div class="spinner-border text-primary" role="status" style="width: 1.5rem; height: 1.5rem;">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-            <div class="mt-2 fw-bold text-dark" style="font-size: 1rem;">Loading...</div>
-        </div>
-
-        <div class="col-12">
-            {{-- <div>
-                <div class="d-flex justify-content-center align-items-center">
-                    <h2 class="mb-5 fw-bold"
-                        style="
-                    font-size: 2.2rem;
-                    text-transform: uppercase;
-                    color: var(--white-color);
-                    text-shadow: 2px 1px 2px #887FF1;
-                ">
-                        Select Your Plan
-                    </h2>
+    <section>
+        <div class="container py-3">
+            <div id="subscription-loader" class="d-none position-absolute top-50 start-50 translate-middle text-center"
+                style="z-index: 9999; min-width: 200px;">
+                <div class="spinner-border text-primary" role="status" style="width: 1.5rem; height: 1.5rem;">
+                    <span class="visually-hidden">Loading...</span>
                 </div>
-            </div> --}}
-            <header class="text-center pb-5">
-                <h1 class="text-center mb-3">Simple And Fexible Pricing</h1>
-                <p style="font-size: 18px" class="mb-0">Lightning-fast email setup. Unlimited Inboxes.</p>
-                <p style="font-size: 18px">Our email infrastructure is built for <strong>speed</strong> and <strong>reliability</strong>.</p>
-            </header>
-        </div>
-
-        <div class="">
-            @if (count($plans) > 0)
-                <div id="plans-container" class="plans px-xl-5">
-                    @foreach ($plans as $plan)
-                        <div id="plan-{{ $plan->id }}">
-                            <div
-                                class="pricing-card h-100 {{ $getMostlyUsed && $plan->id === $getMostlyUsed->id ? '' : '' }}">
-                                <div class="inner-content d-flex flex-column justify-content-between">
-                                    <div>
-                                        {{-- <div class="d-flex align-items-center justify-content-center mb-0">
-                                            <div class="plan-header">
-                                                <h6 class="fs-6 text-uppercase fw-bold">{{ $plan->name }}</h6>
-                                            </div>
-                                        </div> --}}
+                <div class="mt-2 fw-bold text-dark" style="font-size: 1rem;">Loading...</div>
+            </div>
+    
+            <div class="col-12">
+                {{-- <div>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h2 class="mb-5 fw-bold"
+                            style="
+                        font-size: 2.2rem;
+                        text-transform: uppercase;
+                        color: var(--white-color);
+                        text-shadow: 2px 1px 2px #887FF1;
+                    ">
+                            Select Your Plan
+                        </h2>
+                    </div>
+                </div> --}}
+                <header class="text-center pb-5">
+                    <h1 class="text-center mb-3">Simple And Fexible Pricing</h1>
+                    <p style="font-size: 18px" class="mb-0">Lightning-fast email setup. Unlimited Inboxes.</p>
+                    <p style="font-size: 18px">Our email infrastructure is built for <strong>speed</strong> and <strong>reliability</strong>.</p>
+                </header>
+            </div>
+    
+            <div class="">
+                @if (count($plans) > 0)
+                    <div id="plans-container" class="plans px-xl-5">
+                        @foreach ($plans as $plan)
+                            <div id="plan-{{ $plan->id }}">
+                                <div
+                                    class="pricing-card h-100 {{ $getMostlyUsed && $plan->id === $getMostlyUsed->id ? '' : '' }}">
+                                    <div class="inner-content d-flex flex-column justify-content-between">
                                         <div>
-                                            <h4 style="font-size: 28px; font-weight: 500" class="text-white plan-name text-capitalize">
-                                                {{ $plan->name }}
-                                            </h4>
-                                            <div class="mb-3 ">
-                                                <span style="font-family: Montserrat">
-                                                    <span style="font-family: Montserrat">{{ $plan->min_inbox }}
-                                                        {{ $plan->max_inbox == 0 ? '+' : '- ' . $plan->max_inbox }}</span>
-                                                    Inboxes
-                                                </span>
+                                            {{-- <div class="d-flex align-items-center justify-content-center mb-0">
+                                                <div class="plan-header">
+                                                    <h6 class="fs-6 text-uppercase fw-bold">{{ $plan->name }}</h6>
+                                                </div>
+                                            </div> --}}
+                                            <div>
+                                                <h4 style="font-size: 28px; font-weight: 500" class="text-white plan-name text-capitalize">
+                                                    {{ $plan->name }}
+                                                </h4>
+                                                <div class="mb-3 ">
+                                                    <span style="font-family: Montserrat">
+                                                        <span style="font-family: Montserrat">{{ $plan->min_inbox }}
+                                                            {{ $plan->max_inbox == 0 ? '+' : '- ' . $plan->max_inbox }}</span>
+                                                        Inboxes
+                                                    </span>
+                                                </div>
+    
+                                                {{-- <small class="plan-description text-capitalize opacity-75"
+                                                    style="line-height: 1px !important">{{ $plan->description }}</small> --}}
+                                                <h2 style="font-family: Space Grotesk; font-size: 56px;" class="fw-bold plan-price theme-text mb-4 d-flex gap-1 align-items-center mb-0">
+                                                    ${{ number_format($plan->price, 2) }}
+                                                    <span class="fw-light text-white mt-3 opacity-75" style="font-size: 17px; font-family: Space Grotesk;">
+                                                        /{{ $plan->duration == 'monthly' ? 'mo' : $plan->duration }}
+                                                        per Inboxes
+                                                    </span>
+                                                </h2>
+                                                <ul class="list-unstyled features-list">
+                                                    @foreach ($plan->features as $feature)
+                                                        <li style="font-size: 16px; margin-bottom: 12px"
+                                                            class="d-flex align-items-center gap-2">
+                                                            <div>
+                                                                <img src="https://cdn.prod.website-files.com/68271f86a7dc3b457904455f/682b27d387eda87e2ecf8ba5_checklist%20(1).png"
+                                                                    width="25" alt="">
+                                                            </div>
+                                                            {{ $feature->title }} {{ $feature->pivot->value }}
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
                                             </div>
-
-                                            {{-- <small class="plan-description text-capitalize opacity-75"
-                                                style="line-height: 1px !important">{{ $plan->description }}</small> --}}
-                                            <h2 style="font-family: Space Grotesk; font-size: 56px;" class="fw-bold plan-price theme-text mb-4 d-flex gap-1 align-items-center mb-0">
-                                                ${{ number_format($plan->price, 2) }}
-                                                <span class="fw-light text-white mt-3 opacity-75" style="font-size: 17px; font-family: Space Grotesk;">
-                                                    /{{ $plan->duration == 'monthly' ? 'mo' : $plan->duration }}
-                                                    per Inboxes
-                                                </span>
-                                            </h2>
-                                            <ul class="list-unstyled features-list">
-                                                @foreach ($plan->features as $feature)
-                                                    <li style="font-size: 16px; margin-bottom: 12px"
-                                                        class="d-flex align-items-center gap-2">
-                                                        <div>
-                                                            <img src="https://cdn.prod.website-files.com/68271f86a7dc3b457904455f/682b27d387eda87e2ecf8ba5_checklist%20(1).png"
-                                                                width="25" alt="">
-                                                        </div>
-                                                        {{ $feature->title }} {{ $feature->pivot->value }}
-                                                    </li>
-                                                @endforeach
-                                            </ul>
+    
+                                            <button class="btn btn-primary py-2 border-0 subscribe-btn w-100 mt-4"
+                                                data-plan-id="{{ $plan->id }}">
+                                                <span class="btn-text">Subscribe Now</span>
+                                                <span class="spinner-border spinner-border-sm d-none ms-2" role="status"
+                                                    aria-hidden="true"></span>
+                                            </button>
                                         </div>
-
-                                        <button class="btn btn-primary py-2 border-0 subscribe-btn w-100 mt-4"
-                                            data-plan-id="{{ $plan->id }}">
-                                            <span class="btn-text">Subscribe Now</span>
-                                            <span class="spinner-border spinner-border-sm d-none ms-2" role="status"
-                                                aria-hidden="true"></span>
-                                        </button>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                <div class="alert mt-5 p-4 text-center" style="background-color: var(--second-primary)">
-                    <p
-                        style="font-family: 'Poppins', sans-serif; font-weight: 900; font-size: 1.6rem; letter-spacing: 2px; text-transform: uppercase; margin: 0; color: #000000; text-shadow: 1px 1px 1px rgba(0,0,0,0.1);">
-                        No plans available at the moment.
-                    </p>
-                    <p
-                        style="font-family: 'Poppins', sans-serif; font-weight: 700; font-size: 1.1rem; margin-top: 15px; color: #000000;">
-                        Please check back later.
-                    </p>
-                </div>
-            @endif
+                        @endforeach
+                    </div>
+                @else
+                    <div class="alert mt-5 p-4 text-center" style="background-color: var(--second-primary)">
+                        <p
+                            style="font-family: 'Poppins', sans-serif; font-weight: 900; font-size: 1.6rem; letter-spacing: 2px; text-transform: uppercase; margin: 0; color: #000000; text-shadow: 1px 1px 1px rgba(0,0,0,0.1);">
+                            No plans available at the moment.
+                        </p>
+                        <p
+                            style="font-family: 'Poppins', sans-serif; font-weight: 700; font-size: 1.1rem; margin-top: 15px; color: #000000;">
+                            Please check back later.
+                        </p>
+                    </div>
+                @endif
+            </div>
         </div>
-    </div>
+    </section>
 
     @push('scripts')
         <script>
