87ea171dc417bb140514509a1a0506a19db94caf
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-06-30 11:38:26 +0500

    Fixed dashboard counter and history table shows last 5 orders

--- routes/web.php
+++ routes/web.php
@@ -305,6 +305,7 @@ Route::middleware(['custom_role:4'])->prefix('contractor')->name('contractor.')-
     Route::post('/order/panel/email/bulk-import', [ContractorOrderController::class, 'orderSplitImportProcess'])->name('order.panel.email.bulkImport');
     
     Route::get('/dashboard', [App\Http\Controllers\Contractor\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/orders-history', [App\Http\Controllers\Contractor\DashboardController::class, 'getOrdersHistory'])->name('orders.history');
     
     Route::get('/pricing', function () {
         return view('contractor.pricing.pricing');
