cf98897fad14f36ef5424b9ec88c53323aa1786e
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-07-26 14:11:45 +0500

    Update subscription-success.blade.php

--- resources/views/customer/plans/subscription-success.blade.php
+++ resources/views/customer/plans/subscription-success.blade.php
@@ -32,7 +32,7 @@
                         </div>
 
                         <div class="d-flex gap-3 justify-content-center">
-                            <a href="{{ route('customer.orders') }}" class="btn btn-outline-primary d-flex align-items-center ">
+                            <a href="{{ route('customer.order.edit', $order_id) }}" class="btn btn-outline-primary d-flex align-items-center ">
                                 <i class="ti ti-box fs-5 me-2"></i> Proceed With Your Order
                             </a>
                         </div>
