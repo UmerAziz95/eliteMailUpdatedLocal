15f05899a85476b3411d1dc1c60006e0c123d6e0
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-07-30 10:27:17 +0500

    added functionlaity auto increment start from 1000

--- database/migrations/2025_04_18_091255_create_reorder_infos_table.php
+++ database/migrations/2025_04_18_091255_create_reorder_infos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -39,6 +39,8 @@ return new class extends Migration
             $table->string('coupon_code')->nullable();
             $table->timestamps();
         });
+        // Set auto-increment starting value
+        DB::statement("ALTER TABLE reorder_infos AUTO_INCREMENT = 1000;");
     }
 
     /**
