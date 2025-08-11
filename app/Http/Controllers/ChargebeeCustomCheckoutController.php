c49f238b7a5dd7e8422f81d185c5d6e4d974c48d
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-08-11 17:42:37 +0500

    Update ChargebeeCustomCheckoutController.php

--- app/Http/Controllers/ChargebeeCustomCheckoutController.php
+++ app/Http/Controllers/ChargebeeCustomCheckoutController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\CustomCheckoutId;
+use App\Models\CustomCheckOutId;
 use App\Services\ChargebeeCustomCheckoutService;
 use App\Models\Plan;
 //
