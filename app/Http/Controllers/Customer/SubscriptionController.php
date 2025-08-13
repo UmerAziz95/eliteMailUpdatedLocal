4da2e39777280872e5dec22f71d85d13ae12ee73
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-08-13 12:14:57 +0500

    Fixed csv all side

--- app/Http/Controllers/Customer/SubscriptionController.php
+++ app/Http/Controllers/Customer/SubscriptionController.php
@@ -171,22 +171,8 @@ class SubscriptionController extends Controller
                 })
                 ->addColumn('action', function ($subscription) {
                     return '<div class="dropdown">
-                                <button class="p-0 bg-transparent border-0"
-                                        type="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false">
-
-                                    <div class="actions d-flex align-items-center justify-content-between position-relative">
-                                        <div class="board d-flex justify-content-start ps-2" style="background-color: var(--secondary-color); height: 18px;">
-                                            <span class="text-white">Click</span>
-                                        </div>
-
-                                        <div class="action-icon"
-                                            style="position: absolute; left: 0; top: -1px; z-index: 2; background-color: orange; height: 20px; width: 20px; border-radius: 50px; display: flex; align-items: center; justify-content: center;">
-                                            <i class="fa-solid fa-chevron-right text-dark font-bold"></i>
-                                        </div>
-
-                                    </div>
+                                <button class="p-0 bg-transparent border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-ellipsis-vertical"></i>
                                 </button>
                                 
                                 <ul class="dropdown-menu">
--- resources/views/customer/invoices/actions.blade.php
+++ resources/views/customer/invoices/actions.blade.php
@@ -1,18 +1,6 @@
 <div class="dropdown">
     <button class="p-0 bg-transparent border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-
-        <div class="actions d-flex align-items-center justify-content-between position-relative">
-            <div class="board d-flex justify-content-start ps-2"
-                style="background-color: var(--secondary-color); height: 18px;">
-                <span class="text-white">Click</span>
-            </div>
-
-            <div class="action-icon"
-                style="position: absolute; left: 0; top: -1px; z-index: 2; background-color: orange; height: 20px; width: 20px; border-radius: 50px; display: flex; align-items: center; justify-content: center;">
-                <i class="fa-solid fa-chevron-right text-dark font-bold"></i>
-            </div>
-
-        </div>
+        <i class="fa-solid fa-ellipsis-vertical"></i>
     </button>
     <ul class="dropdown-menu">
         <li>
