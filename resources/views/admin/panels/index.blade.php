9776818ec4ac4313dfd3a1c463a07ecd958ff945
ArsamJaved <arsamjaved987.aj@gmail.com>
2025-08-14 15:56:31 +0500

    Update index.blade.php

--- resources/views/admin/panels/index.blade.php
+++ resources/views/admin/panels/index.blade.php
@@ -1699,7 +1699,7 @@
                                                                 onclick="window.location.href='/admin/orders/${order.order_panel_id}/split/view'">
                                                                 View
                                                             </button>
-                                                            ${order.order_status === 'cancelled' || order.order_status === 'reject' ? '' : `
+                                                            ${order.order_status === 'cancelled' || order.order_status === 'reject' || order.order_status === 'removed' ? '' : `
                                                                 <button style="font-size: 12px" class="btn border-0 btn-sm py-0 px-2 rounded-1 btn-success"
                                                                     onclick="openReassignModal(${order.order_id}, ${order.panel_id}, ${order.order_panel_id}, '${panel?.title || 'N/A'}')"
                                                                     title="Reassign to another panel">
