9f08180ada4b47bca511b4a873ba5d2deaafbc8d
UmerAziz95 <salesat5dsolutions@gmail.com>
2025-10-31 11:19:12 +0500

    Update index.blade.php

--- resources/views/customer/pool-orders/index.blade.php
+++ resources/views/customer/pool-orders/index.blade.php
@@ -1,7 +1,7 @@
 @extends('customer.layouts.app')
 
 @section('content')
-<div class="container py-5">
+<div class="">
     <div class="row">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center mb-4">
