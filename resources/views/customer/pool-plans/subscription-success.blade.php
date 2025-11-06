cd2ee112cdb24046323a2b4fabd564e99089bec1
UmerAziz95 <salesat5dsolutions@gmail.com>
2025-11-06 16:59:33 +0500

    Update subscription-success.blade.php

--- resources/views/customer/pool-plans/subscription-success.blade.php
+++ resources/views/customer/pool-plans/subscription-success.blade.php
@@ -9,20 +9,20 @@
                         <div class="mb-4">
                             <i class="fas fa-check-circle text-success" style="font-size: 4rem;"></i>
                         </div>
-                        <h2 class="mb-4 text-white">Pool Plan Subscription Successful! 🎉</h2>
+                        <h2 class="mb-4 text-white">Trial Payment Successful! 🎉</h2>
                         <div style="background-color: #0bf04b2e; border: 1px solid #0bf04b;" class="mb-4 py-3 px-2 rounded-2">
-                            <p class="mb-0 success ">Thank you for subscribing to our Pool Plan! Your account has been successfully upgraded.</p>
+                            <p class="mb-0 success ">Thank you for joining our Trial Plan! For 1 Week.</p>
                         </div>
                         
                         <div class="row mb-4">
-                            <div class="col-md-6">
+                            <div class="col-md-6 d-none">
                                 <div class="p-3 rounded-2" style="background-color: #0bf04b2e; border: 1px solid #0bf04b;">
                                     <h6 class="success mb-2 text-uppercase fw-bold text-shadow">Subscription Details</h6>
                                     <p class="mb-1 success opacity-50"><strong>Subscription ID:</strong> {{ $poolOrder->chargebee_subscription_id }}</p>
                                     <p class="mb-1 success opacity-50"><strong>Pool Order ID:</strong> {{ $poolOrder->id }}</p>
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="p-3 rounded-2" style="background-color: #0bf04b2e; border: 1px solid #0bf04b;">
                                     <h6 class="success mb-2 text-uppercase fw-bold text-shadow">Pool Plan Information</h6>
                                     <p class="mb-1 success opacity-50"><strong>Pool:</strong> {{ $poolPlan->name ?? 'N/A' }}</p>
@@ -46,7 +46,7 @@
                                 <i class="ti ti-eye fs-5 me-2"></i> View Pool Order Details
                             </a> -->
                             <a href="{{ route('customer.pool-orders.edit', $poolOrder->id) }}" class="btn btn-outline-warning d-flex align-items-center">
-                                <i class="ti ti-pencil fs-5 me-2"></i> Select Trai Inboxes
+                                <i class="ti ti-pencil fs-5 me-2"></i> Select Trial Inboxes
                             </a>
                         </div>
                     </div>
